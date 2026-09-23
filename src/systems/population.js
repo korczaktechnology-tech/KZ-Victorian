60135028f2aa3e7d32d47d4fed096da117d8a8f8
kztech <korczaktechnology@gmail.com>
2026-09-23 01:44:14 -0300

    Remove atomic memory write from population

--- src/systems/population.js
+++ src/systems/population.js
@@ -3,11 +3,11 @@ import { EntityType, EntityTypeCode } from "../core/entities.js";
 export function updatePopulation(world) {
   const previous = world.metrics.population;
   let count = 0;
-  world.entities.query("Needs", "Health").forEach((id) => {
+  world.entities.query("Needs", "Health").forEach(id => {
     if (world.entities.type(id) === EntityTypeCode[EntityType.HABITANT]) count += 1;
   });
   world.metrics.population = count;
-  if (world.memory?.regions.population) Atomics.store(world.memory.regions.population, 0, count);
+  if (world.memory?.regions.population) world.memory.regions.population[0] = count;
   if (previous !== count) world.emit("populationChanged", { population: count });
   return count;
 }
