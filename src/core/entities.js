2f7b2f62036e0a96126a916b4ef3b210144cca32
kztech <korczaktechnology@gmail.com>
2026-09-22 23:00:11 -0300

    feat: criar fundação da aplicação WebLords — src/core/entities.js

---
+++ src/core/entities.js
@@ -0,0 +1,10 @@
+export const EntityType = Object.freeze({
+  HABITANT: "habitant",
+  TREE: "tree",
+  HOUSE: "house",
+  WAREHOUSE: "warehouse",
+  SAWMILL: "sawmill",
+  ROAD: "road",
+  RESOURCE: "resource",
+  ANIMAL: "animal"
+});
